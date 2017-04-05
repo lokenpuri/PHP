4de3a1d19c7defada1164b8465f02d5fd82573e8
Lokendra Puri <loken.puri@hotmail.com>
2017-04-05 08:56:34 +0545

    Update login.php

--- login.php
+++ login.php
@@ -1,28 +1,17 @@
 <form method="POST" action="result.php">
-<table>
-	
-<tr>
-	
-	<td>Enter username: </td>
-	<td><input type="text" name="username"></td>
-</tr>
+	<table>
+		<tr>
+			<td>Enter username: </td>
+			<td><input type="text" name="username"></td>
+		</tr>
 
-<tr>
-	
-	<td>Enter password: </td>
-	<td><input type="password" name="password"></td>
-</tr>
-<br>
-<tr>
-	<td colspan="2"><input type="submit" name="Login" value="Login"></td>
-</tr>
-
-
-
-</table>
-	
-
-
-
-
+		<tr>
+			<td>Enter password: </td>
+			<td><input type="password" name="password"></td>
+		</tr>
+		<br>
+		<tr>
+			<td colspan="2"><input type="submit" name="Login" value="Login"></td>
+		</tr>
+	</table>
 </form>
